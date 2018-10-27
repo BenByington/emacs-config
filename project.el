66d9234fcba9560ff4e6fbe22c279569dea86d3b
Ben Byington <bbyington@pacificbiosciences.com>
2018-10-27 07:49:05 -0700

    made project handle multiple repos

--- project.el
+++ project.el
@@ -1,27 +1,32 @@
-
 (setq bb-project nil)
-(defun ppa-proj()(interactive)(setq bb-project "ppa-proj"))
-(defun cplusplus-proj()(interactive) (setq bb-project "cplusplus-proj"))
 
-(defun proj-match (name) (string= name bb-project))
+;; enable the different peojcts
+(defun proj-ppa()(interactive)(setq bb-project "proj-ppa"))
+(defun proj-cpluspus()(interactive) (setq bb-project "proj-cplusplus"))
 
-(setq ppa-rel-dir "~/primary/Sequel/ppa/build/x86_64/Release_gcc")
-(setq ppa-deb-dir "~/primary/Sequel/ppa/build/x86_64/Debug_gcc")
-(setq cc-rel-dir "~/primary/common/pacbio-cplusplus-api/build/x86_64/Release_gcc")
-(setq cc-deb-dir "~/primary/common/pacbio-cplusplus-api/build/x86_64/Debug_gcc")
+;; helper functions
+(defun pb-base-dir ()(locate-dominating-file default-directory ".git"))
+(defun proj-match (name) (when (pb-base-dir) (string= name bb-project)))
+
+;; Gets our appropriate build dirs, regardless of which repo we're in
+(defun ppa-deb-dir () (concat (pb-base-dir) "Sequel/ppa/build/x86_64/Debug_gcc"))
+(defun ppa-rel-dir () (concat (pb-base-dir) "Sequel/ppa/build/x86_64/Release_gcc"))
+(defun cc-rel-dir  () (concat (pb-base-dir) "common/pacbio-cplusplus-api/build/x86_64/Release_gcc"))
+(defun cc-deb-dir  () (concat (pb-base-dir) "common/pacbio-cplusplus-api/build/x86_64/Debug_gcc"))
 
 (global-set-key (kbd "C-S-p") 'multi-compile-run)
 (setq multi-compile-alist '(
-    ((proj-match "ppa-proj") . (("ReleaseBuild" "make -j12" ppa-rel-dir)
-                 ("DebugBuild" "make -j12" ppa-deb-dir)
-                 ("RunDebug" "make test" ppa-deb-dir)
-                 ("RunRelease" "make test" ppa-rel-dir)
-                 ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." ppa-deb-dir)))
-    ((proj-match "cplusplus-proj") . (("ReleaseBuild" "make -j12" cc-rel-dir)
-                      ("DebugBuild" "make -j12" cc-deb-dir)
-                      ("RunDebug" "make test" cc-deb-dir)
-                      ("RunRelease" "make test" cc-rel-dir)
-                      ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." cc-deb-dir))
+    ((proj-match "proj-ppa") . 
+         (("ReleaseBuild" "make -j12" (ppa-rel-dir))
+          ("DebugBuild" "make -j12" (ppa-deb-dir))
+          ("RunDebug" "make test" (ppa-deb-dir))
+          ("RunRelease" "make test" (ppa-rel-dir))
+          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (ppa-deb-dir)))
+    )
+    ((proj-match "proj-cplusplus") . 
+         (("ReleaseBuild" "make -j12" (cc-rel-dir))
+          ("DebugBuild" "make -j12" (cc-deb-dir))
+          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (cc-deb-dir)))
     )
 ))
 
