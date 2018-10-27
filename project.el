89524fe6a41830a3df6c875e91fa0b1942e178c6
Ben Byington <bbyington@pacificbiosciences.com>
2018-10-27 08:38:22 -0700

    Added the rest of the primary projects

--- project.el
+++ project.el
@@ -3,6 +3,10 @@
 ;; enable the different peojcts
 (defun proj-ppa()(interactive)(setq bb-project "proj-ppa"))
 (defun proj-cpluspus()(interactive) (setq bb-project "proj-cplusplus"))
+(defun proj-seq-common()(interactive) (setq bb-project "proj-seq-common"))
+(defun proj-basecaller()(interactive) (setq bb-project "proj-basecaller"))
+(defun proj-basewriter()(interactive) (setq bb-project "proj-basewriter"))
+(defun proj-acquisition()(interactive) (setq bb-project "proj-acquisition"))
 
 ;; helper functions
 (defun pb-base-dir ()(locate-dominating-file default-directory ".git"))
@@ -13,6 +17,14 @@
 (defun ppa-rel-dir () (concat (pb-base-dir) "Sequel/ppa/build/x86_64/Release_gcc"))
 (defun cc-rel-dir  () (concat (pb-base-dir) "common/pacbio-cplusplus-api/build/x86_64/Release_gcc"))
 (defun cc-deb-dir  () (concat (pb-base-dir) "common/pacbio-cplusplus-api/build/x86_64/Debug_gcc"))
+(defun common-rel-dir  () (concat (pb-base-dir) "Sequel/common/build/x86_64/Release_gcc"))
+(defun common-deb-dir  () (concat (pb-base-dir) "Sequel/common/build/x86_64/Debug_gcc"))
+(defun basecaller-deb-dir () (concat (pb-base-dir) "Sequel/basecaller/build/x86_64_gcc/Debug"))
+(defun basecaller-rel-dir () (concat (pb-base-dir) "Sequel/basecaller/build/x86_64_gcc/Release"))
+(defun basewriter-deb-dir () (concat (pb-base-dir) "Sequel/basewriter/build/x86_64/Debug_gcc"))
+(defun basewriter-rel-dir () (concat (pb-base-dir) "Sequel/basewriter/build/x86_64/Release_gcc"))
+(defun acquisition-deb-dir () (concat (pb-base-dir) "Sequel/acquisition/build/x86_64/Debug_gcc"))
+(defun acquisition-rel-dir () (concat (pb-base-dir) "Sequel/acquisition/build/x86_64/Release_gcc"))
 
 (global-set-key (kbd "C-S-p") 'multi-compile-run)
 (setq multi-compile-alist '(
@@ -27,6 +39,22 @@
          (("ReleaseBuild" "make -j12" (cc-rel-dir))
           ("DebugBuild" "make -j12" (cc-deb-dir))
           ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (cc-deb-dir)))
+    )
+    ((proj-match "proj-seq-common") . 
+         (("DebugBuild" "make -j12" (common-deb-dir))
+          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (common-deb-dir)))
+    )
+    ((proj-match "proj-basecaller") . 
+         (("DebugBuild" "make -j12" (basecaller-deb-dir))
+          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (basecaller-deb-dir)))
+    )
+    ((proj-match "proj-basewriter") . 
+         (("DebugBuild" "make -j12" (basewriter-deb-dir))
+          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (basewriter-deb-dir)))
+    )
+    ((proj-match "proj-acquisition") . 
+         (("DebugBuild" "make -j12" (acquisition-deb-dir))
+          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (acquisition-deb-dir)))
     )
 ))
 
