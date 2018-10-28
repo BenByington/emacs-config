8f848bdb58987bdef9e781a6581341477d512a88
Ben Byington <bbyington@pacificbiosciences.com>
2018-10-28 08:23:50 -0700

    Switched from find-file-in-project to find-file-in-repository

--- emacs-init.el
+++ emacs-init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+(add-to-list 'load-path "~/emacs-config-repo/find-file-in-project")
+
 (load-file "~/emacs-config-repo/windows.el")
 (load-file "~/emacs-config-repo/compile.el")
 (load-file "~/emacs-config-repo/rtags.el")
--- files.el
+++ files.el
@@ -1,7 +1,9 @@
 
-(add-to-list 'load-path "~/.emacs.d/find-file-in-project")
-(require 'find-file-in-project)
-(global-set-key (kbd "C-S-o") 'find-file-in-project-by-selected)
+;;(add-to-list 'load-path "~/.emacs.d/find-file-in-project")
+;;(require 'find-file-in-project)
+;;(global-set-key (kbd "C-S-o") 'find-file-in-project-by-selected)
+(require 'find-file-in-repository)
+(global-set-key (kbd "C-S-o") 'find-file-in-repository)
 
 (global-set-key (kbd "C-S-t") 'treemacs)
 
