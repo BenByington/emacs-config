4479e1f04fe5c80407cc0a627c3415d5eeb49d36
Ben Byington <bbyington@pacificbiosciences.com>
2018-10-28 17:09:13 -0700

    Tweaked build, so compiler and release vs deb is set via state machine

--- project.el
+++ project.el
@@ -1,60 +1,66 @@
-(setq bb-project nil)
-
-;; enable the different peojcts
-(defun proj-ppa()(interactive)(setq bb-project "proj-ppa"))
-(defun proj-cpluspus()(interactive) (setq bb-project "proj-cplusplus"))
-(defun proj-seq-common()(interactive) (setq bb-project "proj-seq-common"))
-(defun proj-basecaller()(interactive) (setq bb-project "proj-basecaller"))
-(defun proj-basewriter()(interactive) (setq bb-project "proj-basewriter"))
-(defun proj-acquisition()(interactive) (setq bb-project "proj-acquisition"))
-
-;; helper functions
 (defun pb-base-dir ()(locate-dominating-file default-directory ".git"))
-(defun proj-match (name) (when (pb-base-dir) (string= name bb-project)))
 
-;; Gets our appropriate build dirs, regardless of which repo we're in
-(defun ppa-deb-dir () (concat (pb-base-dir) "Sequel/ppa/build/x86_64/Debug_gcc"))
-(defun ppa-rel-dir () (concat (pb-base-dir) "Sequel/ppa/build/x86_64/Release_gcc"))
-(defun cc-rel-dir  () (concat (pb-base-dir) "common/pacbio-cplusplus-api/build/x86_64/Release_gcc"))
-(defun cc-deb-dir  () (concat (pb-base-dir) "common/pacbio-cplusplus-api/build/x86_64/Debug_gcc"))
-(defun common-rel-dir  () (concat (pb-base-dir) "Sequel/common/build/x86_64/Release_gcc"))
-(defun common-deb-dir  () (concat (pb-base-dir) "Sequel/common/build/x86_64/Debug_gcc"))
-(defun basecaller-deb-dir () (concat (pb-base-dir) "Sequel/basecaller/build/x86_64_gcc/Debug"))
-(defun basecaller-rel-dir () (concat (pb-base-dir) "Sequel/basecaller/build/x86_64_gcc/Release"))
-(defun basewriter-deb-dir () (concat (pb-base-dir) "Sequel/basewriter/build/x86_64/Debug_gcc"))
-(defun basewriter-rel-dir () (concat (pb-base-dir) "Sequel/basewriter/build/x86_64/Release_gcc"))
-(defun acquisition-deb-dir () (concat (pb-base-dir) "Sequel/acquisition/build/x86_64/Debug_gcc"))
-(defun acquisition-rel-dir () (concat (pb-base-dir) "Sequel/acquisition/build/x86_64/Release_gcc"))
+(setq proj-debug-state t)
+(setq proj-arch "gcc")
+
+(defun proj-arch-gcc ()(interactive) (setq proj-arch "gcc"))
+(defun proj-arch-icc ()(interactive) (setq proj-arch "icc"))
+(defun proj-arch-avx ()(interactive) (setq proj-arch "avx512"))
+(defun proj-arch-k1om ()(interactive)(setq proj-arch "k1om"))
+(defun proj-debug()(interactive) (setq proj-debug-state t))
+(defun proj-release()(interactive) (setq proj-debug-state nil))
+
+(defun proj-generic-build-dirs (proj arch debug)
+    (setq baseDir (pb-base-dir))
+    (setq relDir (concat proj "/"))
+    (cond ((and (string= arch "gcc") debug)       (setq archDir "build/x86_64/Debug_gcc"))
+          ((and (string= arch "icc") debug)       (setq archDir "build/x86_64/Debug"))
+          ((and (string= arch "icc") (not debug)) (setq archDir "build/x86_64/Release'"))
+	  (t                                      (setq archDir "invalid-build-arch"))
+    )
+    (when (file-directory-p (concat baseDir relDir archDir)) 
+          (setq return (concat baseDir relDir archDir)))
+)
+(defun proj-ppa-build-dir (arch debug) (proj-generic-build-dirs "Sequel/ppa" arch debug))
+(defun proj-acq-build-dir (arch debug) (proj-generic-build-dirs "Sequel/acquisition" arch debug))
+(defun proj-bw-build-dir (arch debug) (proj-generic-build-dirs "Sequel/basewriter" arch debug))
+(defun proj-cc-build-dir (arch debug) (proj-generic-build-dirs "Sequel/common" arch debug))
+(defun proj-cplusplus-build-dir (arch debug) (proj-generic-build-dirs "common/pacbio-cplusplus-api" arch debug))
+(defun proj-bc-build-dir (arch debug)
+    (setq baseDir (pb-base-dir))
+    (setq relDir "Sequel/basecaller/")
+    (cond ((string= arch "gcc")    (setq archDir "build/x86_64_gcc/"))
+          ((string= arch "icc")    (setq archDir "build/x86_64/"))
+          ((string= arch "avx512") (setq archDir "build/avx512/"))
+          ((string= arch "k1om")   (setq archDir "build/k1om/"))
+	  (t                       (setq archDir "invalid-arch"))
+    )
+    (if debug (setq buildDir "Debug") (setq buildDir "Release"))
+    (when (file-directory-p (concat baseDir relDir archDir buildDir)) 
+          (setq return (concat baseDir relDir archDir buildDir)))
+)
+
+(defun proj-ppa()        (interactive)(fset 'proj-build-dir 'proj-ppa-build-dir))
+(defun proj-cpluspus()   (interactive)(fset 'proj-build-dir 'proj-cplusplus-build-dir))
+(defun proj-seq-common() (interactive)(fset 'proj-build-dir 'proj-cc-build-dir))
+(defun proj-basecaller() (interactive)(fset 'proj-build-dir 'proj-bc-build-dir))
+(defun proj-basewriter() (interactive)(fset 'proj-build-dir 'proj-bw-build-dir))
+(defun proj-acquisition()(interactive)(fset 'proj-build-dir 'proj-acq-build-dir))
+
+(setq multi-compile-alist '(
+    ((proj-build-dir proj-arch proj-debug-state) .
+	(("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (proj-build-dir "gcc" t))
+	 ("Build" "make -j12" (proj-build-dir proj-arch proj-debug-state))
+	 ("Clean" "make clean" (proj-build-dir proj-arch proj-debug-state))
+	 ("CleanBuild" "make clean; make -j12" (proj-build-dir proj-arch proj-debug-state))
+         ("Run" "pwd" (proj-build-dir proj-arch proj-debug-state)))
+)))
+
+(defun proj-curr-build-dir ()(interactive) (print (proj-build-dir proj-arch proj-debug-state)))
+(defun proj-info ()(interactive) (
+	print (concat "Type: " proj-arch
+		      ", Debug: " (if proj-debug-state "true" "false")
+		      ", BuildDir: " (proj-build-dir proj-arch proj-debug-state)
+)))
 
 (global-set-key (kbd "C-S-p") 'multi-compile-run)
-(setq multi-compile-alist '(
-    ((proj-match "proj-ppa") . 
-         (("ReleaseBuild" "make -j12" (ppa-rel-dir))
-          ("DebugBuild" "make -j12" (ppa-deb-dir))
-          ("RunDebug" "make test" (ppa-deb-dir))
-          ("RunRelease" "make test" (ppa-rel-dir))
-          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (ppa-deb-dir)))
-    )
-    ((proj-match "proj-cplusplus") . 
-         (("ReleaseBuild" "make -j12" (cc-rel-dir))
-          ("DebugBuild" "make -j12" (cc-deb-dir))
-          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (cc-deb-dir)))
-    )
-    ((proj-match "proj-seq-common") . 
-         (("DebugBuild" "make -j12" (common-deb-dir))
-          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (common-deb-dir)))
-    )
-    ((proj-match "proj-basecaller") . 
-         (("DebugBuild" "make -j12" (basecaller-deb-dir))
-          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (basecaller-deb-dir)))
-    )
-    ((proj-match "proj-basewriter") . 
-         (("DebugBuild" "make -j12" (basewriter-deb-dir))
-          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (basewriter-deb-dir)))
-    )
-    ((proj-match "proj-acquisition") . 
-         (("DebugBuild" "make -j12" (acquisition-deb-dir))
-          ("RefreshIndex" "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .; rc -J ." (acquisition-deb-dir)))
-    )
-))
-
