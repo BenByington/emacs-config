1a45b027f7c6886706fa1893574f0d682d2f8385
Ben Byington <bbyington@pacificbiosciences.com>
2018-11-10 14:16:15 -0800

    renamed config directory

--- emacs-init.el
+++ emacs-init.el
@@ -2,14 +2,14 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(add-to-list 'load-path (expand-file-name "~/emacs-config-repo/dependencies/multi-compile"))
+(add-to-list 'load-path (expand-file-name "~/emacs-config/dependencies/multi-compile"))
 
-(load-file "~/emacs-config-repo/windows.el")
-(load-file "~/emacs-config-repo/compile.el")
-(load-file "~/emacs-config-repo/rtags.el")
-(load-file "~/emacs-config-repo/files.el")
-(load-file "~/emacs-config-repo/project.el")
-(load-file "~/emacs-config-repo/formatting.el")
+(load-file "~/emacs-config/windows.el")
+(load-file "~/emacs-config/compile.el")
+(load-file "~/emacs-config/rtags.el")
+(load-file "~/emacs-config/files.el")
+(load-file "~/emacs-config/project.el")
+(load-file "~/emacs-config/formatting.el")
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
   backup-by-copying t    ; Don't delink hardlinks
